18252d529723a220246b865ce18654e2b21a0c6d
kevin <kevin@deepai.org>
2026-06-18 01:20:09 +0000

    Shorten ping pong demo

--- demos/ping_pong.c
+++ demos/ping_pong.c
@@ -1,38 +1,3 @@
 int parent_pid;
 
-int child_actor() {
-    int value;
-    int i;
-    i = 0;
-    while (i < 1) {
-        value = msg_recv();
-        msg_send(parent_pid, value + 1, 0);
-        i = i + 1;
-    }
-    exit(0);
-}
-
-int main() {
-    int child;
-    int value;
-    int i;
-    parent_pid = pid();
-    child = fork();
-    if (child == 0) {
-        child_actor();
-        return 0;
-    }
-    i = 0;
-    value = 0;
-    while (i < 1) {
-        msg_send(child, value, 0);
-        value = msg_recv();
-        i = i + 1;
-    }
-    if (value == 1) {
-        write(1, "ping pong ok\n", 13);
-        return 0;
-    }
-    write(2, "ping pong failed\n", 17);
-    return 1;
-}
+int main(){int child;int value;parent_pid=pid();child=fork();if(child==0){value=msg_recv();msg_send(parent_pid,value+1,0);exit(0);}msg_send(child,0,0);value=msg_recv();if(value==1){write(1,"ping pong ok\n",13);return 0;}write(2,"ping pong failed\n",17);return 1;}
