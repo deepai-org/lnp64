7a9e94cc59dfe8befbf4a45b706ea98aa23affd6
kevin <kevin@deepai.org>
2026-06-19 09:46:58 +0000

    rtl: add cap dup no amplification top test

---
+++ tests/rtl/programs/top_cap_dup_no_amplify.s
@@ -0,0 +1,51 @@
+.data
+cap_arg: .zero 32
+
+.text
+  LI r29, -1
+  LI r10, cap_arg
+
+create_read_dup_child:
+  LI r1, 1
+  ST [r10, 0], r1
+  LI r1, 4
+  ST [r10, 8], r1
+  LI r1, 65
+  ST [r10, 16], r1
+  ST [r10, 24], r0
+  CAP_DUP r11, r10
+  CMP r11, r29
+  BEQ bad
+
+reject_broader_rights:
+  ST [r10, 0], r11
+  LI r1, 5
+  ST [r10, 8], r1
+  LI r1, 3
+  ST [r10, 16], r1
+  ST [r10, 24], r0
+  CAP_DUP r12, r10
+  CMP r12, r29
+  BNE bad
+  ERRNO_GET r13
+  LI r1, 1
+  CMP r13, r1
+  BNE bad
+
+allow_further_narrowing:
+  ST [r10, 0], r11
+  LI r1, 5
+  ST [r10, 8], r1
+  LI r1, 1
+  ST [r10, 16], r1
+  ST [r10, 24], r0
+  CAP_DUP r14, r10
+  CMP r14, r29
+  BEQ bad
+
+done:
+  EXIT r0
+
+bad:
+  LI r1, 1
+  EXIT r1
